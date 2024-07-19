3a4a1724d550d0ffb3047fe96a89c68d2ad4171b
Eliezer Perez <eliezersierraperez@hotmail.com>
2024-07-19 14:05:00 +0200

    Nathalie-mota First Commit

---
+++ wp-content/themes/nathaliemota/front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Nathalie_Mota
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+    We are in the front page
+		
+	</main><!-- #main -->
+
+<?php
+get_footer();
