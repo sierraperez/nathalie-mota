7296e8de5ef416091f744f8d204a7e17d7fc2d6f
Eliezer Perez <eliezersierraperez@hotmail.com>
2024-08-05 23:47:06 +0200

    Change header photo

--- wp-content/themes/nathaliemota/front-page.php
+++ wp-content/themes/nathaliemota/front-page.php
@@ -18,7 +18,7 @@ get_header();
 	<main id="primary" class="site-main">
 	<div class="hero-header" bis_skin_checked="1">
 		<a href="<?php echo esc_url(home_url('/')); ?>">
-			<img class= "hero-header__img"src="<?php echo get_template_directory_uri(); ?>/assets/img/nathalie-11.jpeg" alt="<?php bloginfo('name'); ?>">
+			<img class= "hero-header__img"src="<?php echo get_template_directory_uri(); ?>/assets/img/nathalie-13.jpeg" alt="<?php bloginfo('name'); ?>">
 			<h1 class="hero-header__page-title">Photographe Event</h1>
 		</a>
 	</div>
